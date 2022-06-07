4a0123b614c10c3523cc0b23635410119abba4f1
Adekunle Ismail <adismail4@gmail.com>
2022-06-07 10:57:02 +0100

    Add excape_attempts

--- queries.sql
+++ queries.sql
@@ -1,2 +1,3 @@
 -- animal name ends in "mon"
 SELECT name FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
+SELECT name FROM animals WHERE (neutered IS true AND escape_attempts < 3);
