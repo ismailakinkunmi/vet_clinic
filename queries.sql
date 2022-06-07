fecdd15492117c6355cbb9fbd6d1f9733e94ea7c
Adekunle Ismail <adismail4@gmail.com>
2022-06-07 12:18:35 +0100

    Add all queries sql

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
- SELECT * FROM animals WHERE name like '%mon%';
+ SELECT * FROM animals WHERE name like '%mon';
 SELECT name FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
 SELECT name FROM animals WHERE (neutered IS true AND escape_attempts < 3);
 SELECT date_of_birth FROM animals WHERE name IN('Agumon','Pikachu');
