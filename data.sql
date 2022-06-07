9204718cb303bdbe6c6f36bac295698e45b37fa8
Adekunle Ismail <adismail4@gmail.com>
2022-06-07 21:39:38 +0100

    Add all project requirement

--- data.sql
+++ data.sql
@@ -6,3 +6,10 @@ insert into animals(name, date_of_birth,escape_attempts,neutered,weight_kg) valu
 
 -- query and update animals table
 alter table animals add species varchar;
+insert into animals(name,date_of_birth,escape_attempts,neutered,weight_kg) values('Charmander','2020-2-08',0,false,11);
+insert into animals(name,date_of_birth,escape_attempts,neutered,weight_kg) values('Plantmon','2021-11-15',2,true,5.7);
+insert into animals(name,date_of_birth,escape_attempts,neutered,weight_kg) values('Squirtle','1993-4-02',3,true,12.13);
+insert into animals(name,date_of_birth,escape_attempts,neutered,weight_kg) values('Angemon','2005-6-12',1,true,45);
+insert into animals(name,date_of_birth,escape_attempts,neutered,weight_kg) values('Boarmon','2005-6-07',7,true,20.4);
+insert into animals(name,date_of_birth,escape_attempts,neutered,weight_kg) values('Blossom','1998-10-13',3,true,17);
+insert into animals(name,date_of_birth,escape_attempts,neutered,weight_kg) values('Ditto','2022-5-14',4,true,22);
--- queries.sql
+++ queries.sql
@@ -7,10 +7,46 @@ SELECT * FROM animals WHERE neutered is true;
 SELECT name FROM animals WHERE name !='Gabumon';
  SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3 OR weight_kg = 10.4 OR weight_kg = 17.3;
 
-  -- update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change
+  --in transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change
   begin transaction;
   update animals set species = 'unspecified';
   rollback;
 
+  --in transaction 
   -- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon
+begin;
+update animals set species ='digimon' where name like '%mon';
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+update animals set species ='pokemon' where species is null;
+commit;
 
+--  Inside a transaction delete all records in the animals table, then roll back the transaction
+begin;
+delete from animals *;
+rollback;
+
+
+-- Write queries to answer the following questions:
+
+----How many animals are there?
+select * from animals;
+
+----How many animals have never tried to escape?
+select * from animals where escape_attempts = 0;
+
+---- What is the average weight of animals?
+-15.5500000000000000
+
+----Who escapes the most, neutered or not neutered animals?
+Boarmon
+
+----What is the minimum and maximum weight of each type of animal?
+ species | min | max
+---------+-----+------
+ pokemon | -17 |  -11
+ digimon | -45 | -5.7
+
+ ----What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+  species |        avg
+---------+--------------------
+ pokemon | 3.0000000000000000
