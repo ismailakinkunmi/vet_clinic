66b661e070f2f073845fb0c1c52b4535745480f9
Adekunle Ismail <adismail4@gmail.com>
2022-06-07 17:25:39 +0100

    Add speciec column

--- data.sql
+++ data.sql
@@ -3,3 +3,6 @@ insert into animals(name, date_of_birth,escape_attempts,neutered,weight_kg) valu
 insert into animals(name, date_of_birth,escape_attempts,neutered,weight_kg) values('Gabumon','2018-11-15',2,true,8);
 insert into animals(name, date_of_birth,escape_attempts,neutered,weight_kg) values('Pikachu','2021-01-07',1,false,15.04);
 insert into animals(name, date_of_birth,escape_attempts,neutered,weight_kg) values('Devimon','2017-05-12',5,true,11);
+
+-- query and update animals table
+alter table animals add species varchar;
