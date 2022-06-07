7cbfab6a952e6c854787ae553de1fe00415b2b0d
Adekunle Ismail <adismail4@gmail.com>
2022-06-07 11:56:35 +0100

    Add all queries sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,8 @@
--- animal name ends in "mon"
+ SELECT * FROM animals WHERE name like '%mon%';
 SELECT name FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
 SELECT name FROM animals WHERE (neutered IS true AND escape_attempts < 3);
+SELECT date_of_birth FROM animals WHERE name IN('Agumon','Pikachu');
+SELECT (name,escape_attempts) FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered is true;
+SELECT name FROM animals WHERE name !='Gabumon';
+ SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3 OR weight_kg = 10.4 OR weight_kg = 17.3;
