6488917d5d6139a78f2e21bbe49cb74017d0caaa
Adekunle Ismail <adismail4@gmail.com>
2022-06-08 17:59:49 +0100

    Add owners and species table

--- schema.sql
+++ schema.sql
@@ -2,3 +2,7 @@
 create database vet_clinic;
 create table animals(id INT GENERATED ALWAYS AS IDENTITY, name TEXT, date_of_birth date,escape_attempts INT,neutered boolean,weight_kg DECIMAL, PRIMARY KEY(id));
 alter table animals add species varchar;
+
+---create tables for owners and species
+ create table owners(id int generated always as identity primary key, full_name varchar, age int);
+ create table spcies(id int generated always as identity primary key, name varchar);
